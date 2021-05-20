a4c08dd3952c5460158d0319a9cabf026e515f60
Vladislav Gruzdov <78535650+VodzurGoNe@users.noreply.github.com>
2021-05-20 21:41:16 +0400

    Add files via upload

---
+++ telegram_bot/src/main/java/org/gruzdov/springboot/telegram/bot/telegram_bot/TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package org.gruzdov.springboot.telegram.bot.telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
+
+}
